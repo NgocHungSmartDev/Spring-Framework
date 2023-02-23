25c6ac8cc531629d14915afdd53ccaf554c44cc4
NgocHungSmartDev <hoang.ngoc.hungit@gmail.com>
2023-02-23 20:23:56 +0700

    Spring Event | Apply Async

--- spring-event/src/main/java/com/ngochung/springevent/MyDog.java
+++ spring-event/src/main/java/com/ngochung/springevent/MyDog.java
@@ -1,11 +1,13 @@
 package com.ngochung.springevent;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyDog {
 
+    @Async
     @EventListener
     public void doorBellEventListener(DoorBellEvent doorBellEvent) throws InterruptedException {
         Thread.sleep(1000);
