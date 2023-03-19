7a3ea5ee03865e660a15d5bb8640188d5446b2ef
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-03-19 14:15:43 +0700

    Spring Security | init basic project

---
+++ security-jwt/src/main/java/com/ngochung/securityjwt/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.ngochung.securityjwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome to Spring Security without Security";
+    }
+}
