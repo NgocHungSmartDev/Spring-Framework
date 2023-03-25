744be33d391bfd7f3bd9f2c2a46152f8d707a80b
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-03-25 23:10:14 +0700

    Add new entities

---
+++ security-jwt/src/main/java/com/ngochung/securityjwt/entity/Authority.java
@@ -0,0 +1,27 @@
+package com.ngochung.securityjwt.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "authorities")
+public class Authority {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+}
